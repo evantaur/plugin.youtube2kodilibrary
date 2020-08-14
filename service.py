c2cfcc0ea3b2c15a0e0d5ee12d15641231b0e131
evantaur <jani.lehtima@gmail.com>
2020-08-14 05:13:40 +0300

    hotfix to service.py
    hotfix to stop service.py crashing

--- service.py
+++ service.py
@@ -13,7 +13,7 @@ NOW = int(time.time())
 LAST_SCAN=872835240
 
 #wait a little before starting
-time.sleep(5)
+#time.sleep(5)
 
 if xbmcvfs.exists(addon_path+'//config.json'):
     with open(addon_path+'//config.json', 'r') as f:
@@ -28,9 +28,11 @@ if __name__ == '__main__':
         if monitor.waitForAbort(5):
             # Abort was requested while waiting. We should exit
             break
+        #xbmc.log(unicode(time.time()) + ' ' + unicode(LAST_SCAN), level=xbmc.LOGNOTICE)
         if (LAST_SCAN + int(xbmcaddon.Addon().getSetting('update_interval'))*3600) <= int(time.time()) and \
             xbmcaddon.Addon().getSetting('API_key') and \
             xbmcaddon.Addon().getSetting('auto_refresh'):
+            time.sleep(15)
             xbmc.executebuiltin('RunPlugin(plugin://'+addonID+'/?mode=Refresh)')
             LAST_SCAN=int(time.time())
 
